fc0458c3859e7654004451d8dec438b344517c73
ebukathedev <ebuskelele@gmail.com>
2024-03-26 09:05:19 +0100

    add fonts and style assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-	return <h1 className="text-red-600">Hello world</h1>;
+	return (
+		<h1 className="text-red-600 font-lexend text-7xl font-extrabold">
+			Hello world
+		</h1>
+	);
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "1080px",
+			xl: "1200px",
+		},
+		extend: {
+			colors: {
+				green: "#06A290",
+				"brand-green": "#05574D",
+				"light-green": "#DDEEEC",
+				text: "#4A5452",
+				"light-text": "#96A2A0",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				lexend: ["Lexend", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };

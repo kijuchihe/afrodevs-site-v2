4f42fb2c57f0d976771d6e8806fc6fb901a6cc87
ebukathedev <ebuskelele@gmail.com>
2024-03-28 14:50:31 +0100

    start footer section

---
+++ src/containers/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+	return (
+		<section className="bg-white pt-[30px]">
+			<div className="c-container py-8 px-5 flex flex-col gap-12">
+				<div>
+					<h2>What will you build today"</h2>
+					<p>Leverage Afrodev’s community of vetted engineers.</p>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Footer;

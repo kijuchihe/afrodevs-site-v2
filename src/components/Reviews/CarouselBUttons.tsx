afcf3b6167f782db67517553620afc48058b6557
ebukathedev <ebuskelele@gmail.com>
2024-03-30 09:47:09 +0100

    remove animation

--- src/components/Reviews/CarouselBUttons.tsx
+++ src/components/Reviews/CarouselBUttons.tsx
@@ -1,16 +1,15 @@
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi2";
 import { CarouselButtonsProps } from "../../types";
-import AnimButton from "../../animation components/AnimButton";
 
 const CarouselBUttons = ({ onPrevious, onNext }: CarouselButtonsProps) => {
 	return (
 		<div className="carousel-buttons flex gap-4 text-[20px] lg:text-[32px]">
-			<AnimButton onClick={onPrevious}>
+			<button onClick={onPrevious}>
 				<HiArrowLeft />
-			</AnimButton>
-			<AnimButton onClick={onNext}>
+			</button>
+			<button onClick={onNext}>
 				<HiArrowRight />
-			</AnimButton>
+			</button>
 		</div>
 	);
 };

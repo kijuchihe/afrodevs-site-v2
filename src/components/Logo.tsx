9b0497678951ea929659cf69523ff068f989f1fb
ebukathedev <ebuskelele@gmail.com>
2024-04-23 19:24:34 +0100

    Update Logo component to use anchor tag instead of react-router-dom Link

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
 import logo from "../assets/svgs/logo.svg";
 
 const Logo = ({ width }: { width?: string }) => {
 	return (
-		<Link to="/" className={`block ${width || "w-40"}`}>
+		<a href="/" className={`block ${width || "w-40"}`}>
 			<img src={logo} alt="Afrodevs" className="w-full" />
-		</Link>
+		</a>
 	);
 };
 

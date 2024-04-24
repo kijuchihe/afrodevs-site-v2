d5c7de7b73944c64859b96b682642f2b38b15f28
ebukathedev <ebuskelele@gmail.com>
2024-04-24 13:49:08 +0100

    Add react-countup library and update StatusCard component

--- src/components/WorkTogetherSection/StatusCard.tsx
+++ src/components/WorkTogetherSection/StatusCard.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef, useState } from "react";
+import CountUp from "react-countup";
 import FadeUpInView from "../../animation components/FadeUpInView";
 import { StatusCardProps } from "../../types";
-import { useInView } from "framer-motion";
 
 const StatusCard = ({
 	icon,
@@ -10,29 +9,23 @@ const StatusCard = ({
 	bgColor,
 	textColor,
 }: StatusCardProps) => {
-	const [count, setCount] = useState(0);
-	const ref = useRef(null);
-	const isInView = useInView(ref, { once: true });
-
-	useEffect(() => {
-		if (!isInView) return console.log("in view", false);
-
-		if (count < amount) {
-			setCount((prevCount) => prevCount + 1);
-		}
-	}, [isInView, count, amount]);
-
 	return (
 		<FadeUpInView>
 			<div
-				ref={ref}
 				className={`${bgColor} flex items-center p-4 rounded-xl gap-3 min-[624px]:p-6`}
 			>
 				<div className="w-12">
 					<img src={icon} alt="icon" className="w-full" />
 				</div>
 				<div className={`${textColor} gap-[3px]`}>
-					<h4>{count}+</h4>
+					<h4>
+						<CountUp
+							end={amount}
+							duration={2}
+							enableScrollSpy={true}
+						/>
+						+
+					</h4>
 					<div className="font-medium uppercase">{title}</div>
 				</div>
 			</div>
